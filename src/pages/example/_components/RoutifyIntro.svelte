270f8525372b038a114a1e20128532c0aa9d9a1f
7p3m1k <kor0885@naver.com>
2021-01-08 13:39:45 +0900

    meta og test

---
+++ src/pages/example/_components/RoutifyIntro.svelte
@@ -0,0 +1,14 @@
+<head>
+  <title>The Rock (1996)</title>
+  <meta property="og:title" content="The Rock" />
+  <meta property="og:type" content="video.movie" />
+  <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+  <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+  </head>
+
+<h1>Routify Starterddd</h1>
+
+To see an example app, go to 
+<a href="/example">/example</a>
+
+<p>To delete the example app, simply delete the ./src/pages/example folder.</p>
