af414c8d53a3e889f7be134e4394c78e7f03216c
7p3m1k <kor0885@naver.com>
2021-01-08 17:11:02 +0900

    라우티파이 메타태그

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,11 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
-  import Meta from "./meta.svelte";
-	
-	const metadata = {
-		title: 'Svelte is Awesome',
-		description: 'It really is!',
-		image: 'https://svelte.dev/images/twitter-card.png',
-		imageAlt: 'Svelte svelte.dev',
-		url:'svelte.dev'
-	}
+  import { metatags } from '@roxi/routify'
+        metatags.description = 'Routes for Svelte'
+        metatags.title = 'Routify'
+        metatags.image = "https://source.unsplash.com/random"
+        metatags.url = "https://routify.dev/docs/metatags"
 </script>
 
 <style  global>
@@ -17,4 +13,18 @@
 </style>
 
 <Router {routes} />
-<Meta {metadata}/>
+
+
+<meta name="description" content="Routes for Svelte">
+<meta property="og:description" content="Routes for Svelte">
+
+<title>Routify</title>
+<meta name="title" content="Routes for Svelte">
+<meta property="og:title" content="Routes for Svelte">
+
+<meta name="image" content="https://source.unsplash.com/random">
+<meta property="og:image" content="https://source.unsplash.com/random">
+
+
+<meta name="url" content="https://routify.dev/docs/metatags">
+<meta property="og:url" content="https://routify.dev/docs/metatags">
