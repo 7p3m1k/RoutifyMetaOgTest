db358b0c28d971e27c78ca08b890918a94ed346d
7p3m1k <kor0885@naver.com>
2021-01-08 23:20:43 +0900

    metatags 나머지들 테스트

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,10 +2,15 @@
     import { metatags, ready } from '@roxi/routify';
 
     let dataTest;
+    let dataNote;
+    let dataImg;
+
     const getData = async() => {
         const res = await fetch("https://us-central1-allius.cloudfunctions.net/getUserBasicInfo?nickname=david");
         const loo = await res.json()
         dataTest = await loo.name
+        dataNote = await loo.note
+        dataImg = await loo.photo_url
         $ready()
     }
     getData();
@@ -13,7 +18,10 @@
 
 
 <h1>hello</h1>
+
 <svelte:head>
     <title>{dataTest}</title>
+    <description>{dataNote}</description>
+    <img src={dataImg} alt="ss"/>
 </svelte:head>
 
