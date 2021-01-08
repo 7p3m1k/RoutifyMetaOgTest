21d85227457a4fdd109c36871369ea72b276d164
7p3m1k <kor0885@naver.com>
2021-01-08 18:04:12 +0900

    index에 넣기

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,6 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
-  import { metatags } from '@roxi/routify'
-
-  metatags.description = 'Routes for Svelte'
-  metatags.title = 'Routify'
-  metatags.image = "https://source.unsplash.com/random"
-  metatags.url = "https://routify.dev/docs/metatags"
 </script>
 
 <style  global>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,10 @@
 <script>
     import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
     import { metatags } from '@roxi/routify'
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
+    metatags.description = 'Routes for Svelte'
+    metatags.title = 'Routify'
+    metatags.image = "https://source.unsplash.com/random"
+    metatags.url = "https://routify.dev/docs/metatags"
 </script>
 
 <RoutifyIntro />
