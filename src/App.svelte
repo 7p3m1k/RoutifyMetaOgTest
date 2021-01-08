5e90418ed42fad4623d1804cef65ee48eb479bf3
7p3m1k <kor0885@naver.com>
2021-01-08 17:22:29 +0900

    메타태그 밑에꺼 제거

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
   import { metatags } from '@roxi/routify'
-        metatags.description = 'Routes for Svelte'
-        metatags.title = 'Routify'
-        metatags.image = "https://source.unsplash.com/random"
-        metatags.url = "https://routify.dev/docs/metatags"
+
+  metatags.description = 'Routes for Svelte'
+  metatags.title = 'Routify'
+  metatags.image = "https://source.unsplash.com/random"
+  metatags.url = "https://routify.dev/docs/metatags"
 </script>
 
 <style  global>
@@ -13,18 +14,3 @@
 </style>
 
 <Router {routes} />
-
-
-<meta name="description" content="Routes for Svelte">
-<meta property="og:description" content="Routes for Svelte">
-
-<title>Routify</title>
-<meta name="title" content="Routes for Svelte">
-<meta property="og:title" content="Routes for Svelte">
-
-<meta name="image" content="https://source.unsplash.com/random">
-<meta property="og:image" content="https://source.unsplash.com/random">
-
-
-<meta name="url" content="https://routify.dev/docs/metatags">
-<meta property="og:url" content="https://routify.dev/docs/metatags">
