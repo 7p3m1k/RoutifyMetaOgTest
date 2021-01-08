79af5dd45b4b921667c90e1c3be45498a463e100
7p3m1k <kor0885@naver.com>
2021-01-08 16:31:03 +0900

    meta 다른버전

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
-  import { metatags } from '@roxi/routify'
-        metatags.description = 'Routes for Svelte'
-        metatags.title = 'Routify'
-        metatags.image = "rock"
-        metatags.url = "link"
+  import Meta from './Meta.svelte'
+	
+	const metadata = {
+		title: 'Svelte is Awesome',
+		description: 'It really is!',
+		image: 'https://svelte.dev/images/twitter-card.png',
+		imageAlt: 'Svelte svelte.dev',
+		url:'svelte.dev'
+	}
 </script>
 
 <style  global>
@@ -13,18 +17,4 @@
 </style>
 
 <Router {routes} />
-
-<svelte:head>
-  <meta name="description" content="Routes for Svelte">
-  <meta property="og:description" content="Routes for Svelte">
-
-  <meta name="url" content="link">
-  <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-
-  <meta name="image" content="rock">
-  <meta property="og:image" content="https://hedwig-cf.netmarble.com/forum-common/sknightsmmo/sk2/thumbnail/e5ad35cac65842d0a9c1d87ac0068854_1609927010739_d.jpg" />
-
-  <title>Routify</title>
-  <meta name="title" content="Routes for Svelte">
-  <meta property="og:title" content="Routes for Svelte">
-</svelte:head>
+<Meta {metadata}/>
