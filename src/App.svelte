f4e1439135740edad73a43b4ecaaebb61c4f1111
7p3m1k <kor0885@naver.com>
2021-01-08 16:24:17 +0900

     거 svelte:head 추가

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,10 @@
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
   import { metatags } from '@roxi/routify'
-        $: metatags.description = 'Routes for Svelte'
-        $: metatags.title = 'Routify'
-        $: metatags.image = "rock"
-        $: metatags.url = "link"
+        metatags.description = 'Routes for Svelte'
+        metatags.title = 'Routify'
+        metatags.image = "rock"
+        metatags.url = "link"
 </script>
 
 <style  global>
@@ -14,17 +14,17 @@
 
 <Router {routes} />
 
-<head>
+<svelte:head>
   <meta name="description" content="Routes for Svelte">
   <meta property="og:description" content="Routes for Svelte">
 
-  <meta name="url" content="Routes for Svelte">
+  <meta name="url" content="link">
   <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
 
-  <meta name="image" content="Routes for Svelte">
+  <meta name="image" content="rock">
   <meta property="og:image" content="https://hedwig-cf.netmarble.com/forum-common/sknightsmmo/sk2/thumbnail/e5ad35cac65842d0a9c1d87ac0068854_1609927010739_d.jpg" />
 
   <title>Routify</title>
   <meta name="title" content="Routes for Svelte">
   <meta property="og:title" content="Routes for Svelte">
-</head>
+</svelte:head>
