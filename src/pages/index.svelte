3887c80e714d7d8617a5fdb15003924d45c1d8a4
7p3m1k <kor0885@naver.com>
2021-01-08 21:04:06 +0900

    fetch전 중간저장

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,10 +1,23 @@
 <script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@roxi/routify'
-    metatags.description = 'Routes for Svelte'
-    metatags.title = 'Routify'
-    metatags.image = "https://source.unsplash.com/random"
-    metatags.url = "https://routify.dev/docs/metatags"
+import RoutifyIntro from './example/_components/RoutifyIntro.svelte';
+import { metatags } from '@roxi/routify';
+import { onMount } from 'svelte';
+
+    // const fetchTitle = (async()=> {
+    //     const response = await fetch("https://us-central1-allius.cloudfunctions.net/getUserBasicInfo?nickname=david")
+    //     return await response.json()
+    // })();
+
+    onMount(async() => {
+        const res = await fetch("https://us-central1-allius.cloudfunctions.net/getUserBasicInfo?nickname=david");
+        
+    })
+
 </script>
 
 <RoutifyIntro />
+
+<!-- {#await fetchTitle}
+{:then data}
+<h1>{data.name}</h1>
+{/await} -->
