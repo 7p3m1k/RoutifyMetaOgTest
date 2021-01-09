3854a59a7b500b7525567f3e18adf752ea9cf8ca
7p3m1k <kor0885@naver.com>
2021-01-09 13:24:15 +0900

    온마운트 추가 + title 수정

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,28 +1,28 @@
 <script>
-    import { ready } from '@roxi/routify';
-		import { onMount } from 'svelte';
+  import { ready } from '@roxi/routify';
+	import { onMount } from 'svelte';
 
-		let data = {};
+	let data = {};
 
-    const getData = async() => {
-        const res = await fetch("https://us-central1-allius.cloudfunctions.net/getUserBasicInfo?nickname=david");
-        let loo = await res.json()
-				data = loo;
-        $ready()
-		}
-		
-		onMount(() => {
-			getData();
+  const getData = async() => {
+    const res = await fetch("https://us-central1-allius.cloudfunctions.net/getUserBasicInfo?nickname=david");
+    let loo = await res.json()
+    data = loo;
+    $ready()
+	}
+
+	onMount(() => {
+		getData();
 	});
 </script>
 
 
-<h1>hello</h1>
+<h1>배고파...</h1>
 
 <svelte:head>
-    <title>{data.title}</title>
-    <meta property="og:title" content={data.name}/>
-    <meta property="og:image" content={data.photo_url}/>
-    <meta property="og:description" content={data.note}/>
+  <title>{data.name}</title>
+  <meta property="og:title" content={data.name}/>
+  <meta property="og:image" content={data.photo_url}/>
+  <meta property="og:description" content={data.note}/>
 </svelte:head>
 
